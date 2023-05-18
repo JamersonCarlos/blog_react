9ae05df5d8bd4fced3454a3df4fba6ae173664fc
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2023-05-18 01:25:11 -0300

    config

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,39 @@
 import './Dashboard.css'
 
 import React from 'react'
+import { useAuthValue } from '../../context/AuthContext'
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+import { Link } from 'react-router-dom';
+import { useDeleteDocument } from '../../hooks/useDeleteDocument';
 
 function Dashboard() {
+  const { user } = useAuthValue();
+  const uid = user.uid;
+  const {documents: posts } = useFetchDocuments("posts", null, uid);
+  const {deleteDocument} = useDeleteDocument("posts");
+  console.log(posts)
+  
   return (
-    <div>Dashboard</div>
+    <div className='dashboard'>
+      <h1>Dashboard</h1>
+      <p>Gerencie os seus posts</p>
+      <div className='line'>
+        <p>Título</p>
+        <p>Ações</p>
+      </div>
+      {posts && posts.map((post) => (
+        <div key={post.title} className='postConfig'>
+          <p>{post.title}</p>
+          <div className='buttons'>
+            <Link to={`/posts/${post.id}`} className='btn btn-outline'>Ver</Link>
+            <Link to={`/posts/update/${post.id}`} className='btn btn-outline'>Editar</Link>
+            <button onClick={() => deleteDocument(post.id)} className='btn btn-outline btn-danger'>
+              Excluir
+            </button>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
